51675d795dfd9231d3b189b89030d94a521eb41c
Moe Thant Kyaw <moethantk@gmail.com>
2022-10-19 16:16:07 +0700

    add base ion sfu client

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import Sidebar from "./Sidebar";
+import Room from "./components/Room";
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>test react</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <Sidebar/>
+        <div>
+            <Sidebar />
+            <Room />
         </div>
     );
 }
