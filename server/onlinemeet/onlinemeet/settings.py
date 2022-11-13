a06cf3822be15308663f20a7c9eb725b4f197344
Moe Thant Kyaw <moethantk@gmail.com>
2022-11-13 15:36:10 +0700

    local development readme

--- server/onlinemeet/onlinemeet/settings.py
+++ server/onlinemeet/onlinemeet/settings.py
@@ -53,7 +53,7 @@ SECRET_KEY = 'django-insecure-1fmlsf7c)&k85%=s&6@5&n%_#89(%v1@kvrcehw8zgrye5%7-&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.104', '127.0.0.1']
 
 
 # Application definition
